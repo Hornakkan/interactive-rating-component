9793a2d48678a828a4dfc74300924f45566c5345
Hornakkan <lacour.s@evhr.net>
2022-04-30 13:47:48 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const allGrades = document.querySelectorAll('.grade');
+const bouton = document.querySelector('button');
+const yourRating = document.querySelector('.your-rating');
+
+allGrades.forEach(grade => {
+    grade.addEventListener('click', (e) => {
+
+        e.target.classList.add('selected');
+        yourRating.innerHTML = e.target.innerHTML;
+
+    for(let i = 0; i < allGrades.length; i++){
+        if(allGrades[i] !== e.target){
+            allGrades[i].classList.remove('selected');
+        }
+    }
+
+    })
+});
+
+bouton.addEventListener('click', () => {
+    document.querySelector('.rating-process').classList.add('inactive');
+    document.querySelector('.star').classList.add('inactive');
+    document.querySelector('.thanks-process').classList.remove('inactive');
+})
